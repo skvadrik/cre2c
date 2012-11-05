8d99bf3a2a7a65dc01ddda6961a2d112d142b7e9
Ulya Fokanova <skvadrik@gmail.com>
2012-11-05 15:46:52 +0300

    Now multiple .def files allowed
    (providing they do not intersect)

--- cre2c.hs
+++ cre2c.hs
@@ -4,6 +4,8 @@
 import           System.Environment          (getArgs)
 import qualified Data.HashMap.Strict   as M
 import qualified Data.ByteString.Char8 as BS
+import           Data.List                   (foldl')
+import           Control.Applicative         ((<$>))
 
 import           Types
 import           CFA
@@ -24,18 +26,32 @@ gen_code (Chunk code rules chunk_list) k regexp_table =
     in  (BS.append code' code'', max maxlen' maxlen'')
 
 
+merge_regexp_tables :: [RegexpTable] -> RegexpTable
+merge_regexp_tables []         = error "No .def files specified"
+merge_regexp_tables (rt : rts) =
+    let merge_one :: RegexpTable -> RegexpTable -> RegexpTable
+        merge_one = M.foldlWithKey' (\ rt nm r ->  case M.lookup nm rt of
+            Nothing -> M.insert nm r rt
+            Just _  -> error $ concat
+                [ "\nMultiple definitions of the same name found: "
+                , nm
+                , "\nEither remove or rename one of the regexps."
+                ])
+    in  foldl' merge_one rt rts
+
+
 main :: IO ()
 main = do
     args <- getArgs
-    (fsrc, fdest, fre) <- case args of
-        [src, dest, re] -> return (src, dest, re)
-        _               -> error "usage: ./cre2c.hs <source.cre> <destination.cpp> <rules.def>"
+    (fcre, fcpp, fdefs) <- case args of
+        cre : cpp : defs -> return (cre, cpp, defs)
+        _                -> error "usage: ./cre2c.hs <source.cre> <destination.cpp> <rules.def> [<rules1.def> ... <rulesN.def>]"
 
-    chunk_list   <- parse_source fsrc
-    regexp_table <- parse_regexps fre
+    chunk_list   <- parse_source fcre
+    regexp_table <- merge_regexp_tables <$> mapM parse_regexps fdefs
     let (code, maxlen) = gen_code chunk_list 0 regexp_table
 
-    BS.writeFile fdest $ BS.concat
+    BS.writeFile fcpp $ BS.concat
         [ BS.pack "#define MAXLEN "
         , BS.pack $ show maxlen
         , BS.pack "\n"
